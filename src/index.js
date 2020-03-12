1e39aeddbcb4d3fc347912c5f3c87b78e5ed4355
Jair Jaramillo <jair.jaramillo@outlook.com>
2020-03-12 14:08:11 -0600

    Add todo object

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 //import display from './display';
 import project from './project';
+import './css/style.css'
 
 //display.pageSetup();
 project.render();
---
+++ src/todo.js
@@ -0,0 +1,54 @@
+const Todo = ((newTitle = 'New Task', newDesc, newDueDate, newPriority = 2) => {
+  let title = newTitle;
+  let description = newDesc;
+  let dueDate = newDueDate;
+  let priority = newPriority;
+  const creationDate = Date.now();
+
+  const changeTitle = (string) => {
+    if (validateName) {
+      title = string;
+    }
+  }
+
+  const changeDescription = (string) =>  {
+    description = string;
+  }
+
+  const changeDueDate = (string) =>  {
+    if (validateDueDate) {
+      dueDate = string;
+    }
+
+    console.log('ERROR: The due date cannot be before the creation date of the task')
+  }
+
+  const changePriority = (number) =>  {
+    priority = string;
+  }
+
+  const validateName = (string) => {
+    if (string == null) {
+      return false;
+    }
+
+    return true;
+  }
+
+  const validateDueDate = (Date) => {
+    if (Date <= creationDate) {
+      return false;
+    }
+
+    return true;
+  }
+
+  return {
+    changeTitle,
+    changeDescription,
+    changeDueDate,
+    changePriority,
+  }
+})();
+
+export default Todo
